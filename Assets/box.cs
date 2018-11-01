e1990ae3ac690210b6da9ff8d5c153a80c38fe30
coneshot <43498819+coneshot@users.noreply.github.com>
2018-11-01 21:55:50 +0800

    Initial commit

---
+++ Assets/box.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour {
+    public int row;
+    public int col;
+    public GameObject core;
+
+    public void OnMouseDown()
+    {
+
+        int[,] boxes = core.GetComponent<gamecontroller2>().boxes;
+        int flag = core.GetComponent<gamecontroller2>().flag;
+
+        /*if ()//已經被點過了
+        { }*/
+
+        if (flag%2==0) //基數次被點
+        {
+            GetComponent<SpriteRenderer>().sprite = core.GetComponent<gamecontroller2>().icon[0];
+
+            flag += 1;
+        }
+
+        else if (flag%2==1) //偶數次被點
+        {
+            GetComponent<SpriteRenderer>().sprite = core.GetComponent<gamecontroller2>().icon[1];
+            boxes[,]= 2;
+            flag += 1;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/gamecontroller2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamecontroller2 : MonoBehaviour {
+    public Sprite[] icon;
+    public int flag = 0;
+    public int[,] boxes = new int[3, 3];
+    public GameObject NewText;
+
+    // Use this for initialization
+    void Start () 
+    {
+
+
+        if ((boxes[0, 0] == 1 && boxes[0, 1]== 1 && boxes[0, 2] == 1)
+            || (boxes[1, 0] == 1 && boxes[1, 1] == 1 && boxes[1, 2] == 1)
+            || (boxes[2, 0] == 1 && boxes[2, 1] == 1 && boxes[2, 2] == 1)
+            || (boxes[0, 0] == 1 && boxes[1, 0] == 1 && boxes[2, 0] == 1)
+            || (boxes[0, 1] == 1 && boxes[1, 1] == 1 && boxes[2, 1] == 1)
+            || (boxes[0, 2] == 1 && boxes[1, 2] == 1 && boxes[2, 2] == 1)
+            || (boxes[0, 0] == 1 && boxes[1, 1] == 1 && boxes[2, 2] == 1)
+            || (boxes[0, 2] == 1 && boxes[1, 1] == 1 && boxes[2, 0] == 1)
+            )
+        {
+            NewText.GetComponent<TextMesh>().text = ("O Win!");
+        }
+
+        else if ((boxes[0, 0] == 2 && boxes[0, 1] == 2 && boxes[0, 2] == 2)
+            || (boxes[1, 0] == 2 && boxes[1, 1] == 2 && boxes[1, 2] == 2)
+            || (boxes[2, 0] == 2 && boxes[2, 1] == 2 && boxes[2, 2] == 2)
+            || (boxes[0, 0] == 2 && boxes[1, 0] == 2 && boxes[2, 0] == 2)
+            || (boxes[0, 1] == 2 && boxes[1, 1] == 2 && boxes[2, 1] == 2)
+            || (boxes[0, 2] == 2 && boxes[1, 2] == 2 && boxes[2, 2] == 2)
+                 || (boxes[0, 0] == 2 && boxes[1, 1] == 2 && boxes[2, 2] == 2)
+                 || (boxes[0, 2] == 2 && boxes[1, 1] == 2 && boxes[2, 0] == 2))
+        {
+            NewText.GetComponent<TextMesh>().text = ("X Win!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
